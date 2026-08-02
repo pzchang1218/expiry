d9dbc387960b1c21bd1c06a8661d403c7f4e7798
pzchang1218 <pzchang1218@gmail.com>
2026-08-02 18:36:09 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+// 離線快取:第一次上網開啟後,之後沒網路也能用
+const CACHE = 'pos-expiry-v1';
+const ASSETS = [
+  './', './index.html', './manifest.json', './icon-192.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.8/html5-qrcode.min.js'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(
+    ks.filter(k => k !== CACHE).map(k => caches.delete(k))
+  )).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }))
+  );
+});
